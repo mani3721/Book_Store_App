fc8cde1602987e438d9bd1f93cb055b14fe7e027
Gopisakthivel <gopisakthivel@alphabsolutions.com>
2025-01-12 18:51:39 +0530

    book

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+const Layout =({children} : {children:ReactNode})=> {
+
+    return <main className="root-container">
+        <div className="mx-auto max-w-7xl">
+
+        </div>
+    </main>
+
+}
+
+export default Layout;
